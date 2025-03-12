4e3922e8a66e9b6db3ffc3f58a56d74175a515d4
Wilmer Marroquin <ios8.wil@gmail.com>
2025-03-12 08:17:55 -0600

    Creacion de Login

---
+++ app/src/main/java/com/example/appumg/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.example.appumg
+
+import android.app.ProgressDialog
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+
+        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        //  val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        //    insets
+        //}
+    }
+
+
+}
